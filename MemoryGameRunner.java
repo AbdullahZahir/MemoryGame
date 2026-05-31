15da1a151581008c92075d638fccce1da0cf299d
Abdullah Z <91299305+AbdullahZahir@users.noreply.github.com>
2026-05-31 16:23:31 -0400

    Add MemoryGameRunner to initiate the game

---
+++ MemoryGameRunner.java
@@ -0,0 +1,10 @@
+ 
+public class MemoryGameRunner
+{
+    public static void main(String[] args) {
+        MemoryGame game = new MemoryGame();
+        // uncomment the next line once everything is tested and working. 
+        game.play();
+    } // end of main
+    
+} // end of class
